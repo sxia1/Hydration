f11006047e4840669e14dfa57e0ae1ad878c949d
unknown <sxia1@stuy.edu>
2018-04-18 21:16:32 -0400

    Skeletal

--- Deque.java
+++ Deque.java
@@ -1,50 +1,58 @@
 public interface Deque<Card>{
     
-    /* List of ALL Methods listed in the API
+    //List of ALL Methods listed in the API
+
+    //Size Method
+    public int size();
+
     //add methods
     public boolean add(Card c);
     public void addFirst(Card c);
     public void addLast(Card c);
+
     //-----Insertion Methods: seems redundant since it just inserts an element at tail, front or end
-    public boolean offer(Card c);
-    public boolean offerFirst(Card c);
-    public boolean offerLast(Card c);
+    //public boolean offer(Card c);
+    //public boolean offerFirst(Card c);
+    //public boolean offerLast(Card c);
 
     //Search methods
-    public boolean contains(Object o);
+    //doesn't seem to be the most important right now
+    //public boolean contains(Object o);
 
     //Iterator Methods
-    public Iterator<Card> descendingIterator();
-    public Iterator<Card> iterator();
+    //Maybe later?
+    //public Iterator<Card> descendingIterator();
+    //public Iterator<Card> iterator();
 
     //Accessor Methods
     public Card element();
     public Card getFirst();
     public Card getLast();
+
     //-----peek: seems redundant since it just retrieves the values
-    public Card peek();
-    public Card peekFirst();
-    public Card peekLast();
+    //public Card peek();
+    //public Card peekFirst();
+    //public Card peekLast();
 
     //Stack Methods
-    public Card pop();
-    public void push(Card c);
+    //Deque will not be implementing a stack
+    //public Card pop();
+    //public void push(Card c);
 
     //Remove Methods
     public Card remove();
     public Card removeFirst();
     public Card removeLast();
+
     //-----removeObjects: seems complicated
-    public boolean remove(Object o);
-    public boolean removeFirstOccurence(Object o);
-    public boolean removeLastOccurence(Object o);
+    //public boolean remove(Object o);
+    //public boolean removeFirstOccurence(Object o);
+    //public boolean removeLastOccurence(Object o);
+
     //-----poll: seems redundant since it just removes and returns elements
-    public Card poll();
-    public Card pollFirst();
-    public Card pollLast();
+    //public Card poll();
+    //public Card pollFirst();
+    //public Card pollLast();
 
 
-    //Size Method
-    public int size();
-    */
 }
--- QQKachoo.java
+++ QQKachoo.java
@@ -4,11 +4,37 @@ public class QQKachoo<D> implements Deque<D>{
     private DLLNode<D> _front, _end;
     private int _size;
 
-    public boolean add(D d){
-	addLast(d);
-	return true;
+    //Methods--------------------
+    
+    public int size(){
+	return _size;
     }
-    public void addLast(D d){
-	
+    
+    //Accessor Methods
+    public Card element(){
     }
+    public Card getFirst(){
+    }
+    public Card getLast(){
+    }
+
+    //Add Methods
+    public boolean add(Card c){
+    }
+    public void addFirst(Card c){
+    }
+    public void addLast(Card c){
+    }
+
+    //Remove Methods
+    public Card remove(){
+    }
+    public Card removeFirst(){
+    }
+    public Card removeLast(){
+    }
+
+    
+    
+    public boolean contains(Object o){}
 }
