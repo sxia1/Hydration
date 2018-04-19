e4e418d7609c6626de0943262efea45a778ccf01
Maggie <mzhao3@stuy.edu>
2018-04-19 08:16:25 -0400

    STOP GIVING ME MERGE CONFLICTS

--- QQKachoo.java
+++ QQKachoo.java
@@ -6,62 +6,65 @@
 
 public class QQKachoo<D> implements Deque<D>{
 
-    //Instance Variables
+    //INSTANCE VARIABLES =============================================
     private DLLNode<D> _head, _tail;
     private int _size;
 
-    //Methods--------------------
+    //METHODS =========================================================
 
     //Size
     public int size(){
-	return _size;
+	     return _size;
     }
-    
+
     //Accessor Methods
-    public D element(){	
-	return _head.getCargo();
+    public D element(){
+	     return _head.getCargo();
     }
     public D getFirst(){
-	return _head.getCargo();
+	     return _head.getCargo();
     }
     public D getLast(){
-	return _tail.getCargo();
+	     return _tail.getCargo();
     }
 
     //Add Methods
     public boolean add(D d){
-	
+	     addLast();
+       return true;
     }
     public void addFirst(D d){
-	DLLNode<D> newNode= new DLLNode<D>(d, null, _head);
-	_head.setPrev(newNode);
-	_head = newNode;
+      	DLLNode<D> newNode= new DLLNode<D>(d, null, _head);
+      	_head.setPrev(newNode);
+      	_head = newNode;
     }
     public void addLast(D d){
-	DLLNode<D> newNode = new DLLNode<D>(d, _tail, null);
-	_tail.setNext(newNode);
-	_tail = newNode;
+      	DLLNode<D> newNode = new DLLNode<D>(d, _tail, null);
+      	_tail.setNext(newNode);
+      	_tail = newNode;
     }
 
     //Remove Methods
     public D remove(){
-	
+      return removeFirst();
     }
+
     public D removeFirst(){
-	D temp = _head.getCargo();
-	_head = _head.getNext();
-	_head.setPrev(null);
-	return temp;
+    	D temp = _head.getCargo();
+    	_head = _head.getNext();
+    	_head.setPrev(null);
+    	return temp;
     }
+
     public D removeLast(){
-	D temp = _tail.getCargo();
-	_tail = _tail.getPrev();
-	_tail.setNext(null);
-	return temp;
+    	D temp = _tail.getCargo();
+    	_tail = _tail.getPrev();
+    	_tail.setNext(null);
+    	return temp;
     }
 
     public static void main(String[] args){
-	
+
     }
-    
+
 }
