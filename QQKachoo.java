c94fde9a0408594549597351c7d81b9d1616c637
unknown <sxia1@stuy.edu>
2018-04-25 21:54:29 -0400

    edits

--- QQKachoo.java
+++ QQKachoo.java
@@ -16,50 +16,65 @@ public class QQKachoo<D> implements Deque<D>{
 
     //METHODS =========================================================
 
-    //Constructor
+    //Constructor------------------------------------
     public void QQKachoo() {
 	_head = new DLLNode(null,null,null);
 	_tail = new DLLNode(null,null,null);
 	_size = 0;
     }
 
+    //Accessors--------------------------------------
     //Returns the number of elements in this deque.
     public int size(){
 	return _size;
     }
 
-    //Retrieves, but does not remove, the head of the queue represented by this deque (in other words, the first element of this deque). Throws a NoSuchElementException if the deque is empty.
-    // * Throw exception.
+    //Retrieves, but does not remove, the head of the queue represented by this deque (in other words, the first element of this deque).
+    //Throws a NoSuchElementException if the deque is empty.
     public D element(){
+	if (_size == 0)
+	    throw new NoSuchElementException();
 	return _head.getCargo();
     }
 
     //Retrieves, but does not remove, the first element of this deque.
+    //Throws a NoSuchElementException if the deque is empty.
     public D getFirst(){
+	if (_size == 0)
+	    throw new NoSuchElementException();
 	return _head.getCargo();
     }
 
     //Retrieves, but does not remove, the last element of this deque.
+    //Throws a NoSuchElementException if the deque is empty.
     public D getLast(){
+	if (_size == 0)
+	    throw new NoSuchElementException();
 	return _tail.getCargo();
     }
 
+    //retrieves, but does not remove, the first element of this deque
     public D peek() {
-	return peekFirst();
+	return peekFirst();	    
     }
 
+    //retrieves, but does not remove, the first element of this deque
+    //returns null if empty
     public D peekFirst() {
 	if (_size == 0)
 	    return null;
 	return _head.getCargo();
     }
 
+    //retrieves, but does not remove, the last element of this deque    
+    //returns null if empty
     public D peekLast() {
 	if (_size == 0)
 	    return null;
 	return _tail.getCargo();
     }
 
+    //Add Methods-----------------------------------------------
     //Inserts the specified element into the queue represented by this deque (in other words, at the tail of this deque) if it is possible to
     public boolean add(D d){
 	addLast(d);
@@ -69,7 +84,7 @@ public class QQKachoo<D> implements Deque<D>{
     //Inserts the specified element at the front of this deque if it is possible to do so immediately without violating capacity restrictions
     public void addFirst(D d){
       	DLLNode<D> newNode= new DLLNode<D>(d, null, _head);
-        if (size() == 0)
+        if (_size == 0)
 	    _head = _tail = newNode;
         else {
 	    newNode.setNext(_head);
@@ -82,9 +97,10 @@ public class QQKachoo<D> implements Deque<D>{
     //Inserts the specified element at the end of this deque if it is possible to do so immediately without violating capacity restrictions.
     public void addLast(D d){
       	DLLNode<D> newNode = new DLLNode<D>(d, _tail, null);
-        if (size() == 0)
+        if (_size == 0){
 	    _head = _tail = newNode;
-        else {
+	}
+	else{
 	    _tail.setNext(newNode);
 	    newNode.setPrev(_tail);
 	    _tail = newNode;
@@ -92,33 +108,35 @@ public class QQKachoo<D> implements Deque<D>{
 	_size++;
     }
 
-    //Remove Methods
+    //Remove Methods-----------------------------------------------
+    // Removes the first element of this Deque
     public D remove(){
 	return removeFirst();
     }
 
+    // Removes the first element of this Deque
     // throws an exception if empty
     public D removeFirst(){
 	if (_size == 0)
 	    throw new NoSuchElementException();
-
-    	D temp = _head.getCargo();
+	D temp = _head.getCargo();
 	if (_size > 1)
 	    _head = _head.getNext();
-    	_head.setPrev(null);
+	_head.setPrev(null);
 	_size--;
-    	return temp;
+	return temp;
     }
 
+    // Removes the last element of this Deque
     // throws an exeption if empty
     public D removeLast(){
 	if (_size == 0)
 	    throw new NoSuchElementException();
-    	D temp = _tail.getCargo();
-    	_tail = _tail.getPrev();
-    	_tail.setNext(null);
+	D temp = _tail.getCargo();
+	_tail = _tail.getPrev();
+	_tail.setNext(null);
 	_size--;
-    	return temp;
+	return temp;
     }
 
     /*
@@ -141,15 +159,4 @@ public class QQKachoo<D> implements Deque<D>{
     }
     */
     
-    public static void main(String[] args){
-	QQKachoo deck = new QQKachoo();
-        deck.addFirst("boo");
-        deck.addLast("hoo");
-        deck.addFirst("who");
-	System.out.println(deck.peekFirst());//who
-        System.out.println(deck.removeFirst());//who
-        System.out.println(deck.removeFirst());//boo
-	System.out.println(deck.peekFirst());//hoo
-        System.out.println(deck.removeFirst());//hoo
-    }
 }
